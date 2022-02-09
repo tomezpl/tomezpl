8c75a4e06b602f89bbdbdd0841c4d81f738bf936
Tomasz Zając <tomezpl@gmail.com>
2022-02-09 20:43:35 +0000

    roombarumble

--- src/Showcase.vue
+++ src/Showcase.vue
@@ -151,7 +151,7 @@
                     id: 0,
                     title: 'Car Physics',
                     githubLink: '',
-                    tags: ['C#', 'C++', 'Unity3D', 'Unreal Engine 4', 'physics programming', 'gameplay programming'],
+                    tags: ['C#', 'C++', 'Unity3D', 'Unreal Engine 4', 'physics programming', 'gameplay programming', 'see end for GitHub links'],
                     content: `
                         <p>
                             These were two spare-time projects that I used as opportunities to learn implementing vehicle physics in a 3D video game.
@@ -208,6 +208,10 @@
                         <p class="mt-3">
                             Excuse the shaky camera - aside from the fact that this is not a complete solution, the camera is also attached to the car object, which means even the smallest motion impacts the view.
                         </p>
+                        <p>
+                            Source code for the UE4 implementation: <a href="https://github.com/tomezpl/UE4Tut/blob/master/Source/UE4Tut/CarWheel.h" target="_blank"><code>Source/CarWheel.[h|cpp]</code><i class="bi-box-arrow-up-right tiny-icon align-top"></i></a>,
+                            <a href="https://github.com/tomezpl/UE4Tut/blob/master/Source/UE4Tut/TppCarPawn.h" target="_blank"><code>Source/TppCarPawn.[h|cpp]</code><i class="bi-box-arrow-up-right tiny-icon align-top"></i></a>.
+                        </p>
                     `
                 },
                 {
@@ -215,8 +219,158 @@
                     id: -1,
                     title: 'RoombaRumble',
                     githubLink: '',
-                    tags: [],
-                    content: ``
+                    tags: ['C#', 'Unity3D', 'URP', 'HLSL', 'ShaderGraph', 'MLAPI', 'game systems & gameplay programming', 'physics programming', 'multiplayer programming', 'UI programming', 'game design', 'vfx'],
+                    content: `
+                        <p>
+                            Originally intended as an entry for the <a href="https://itch.io/jam/7dfps-2020" target="_blank">7DFPS 2020<i class="bi-box-arrow-up-right tiny-icon align-top"></i></a> game jam,
+                            this has evolved into a personal project I've been putting a lot of time and effort into (as much time as you can reasonably afford as a working person, that is).
+                        </p>
+                        <p>
+                            The game is a silly multiplayer third-person shooter (originally FPS, as per the game jam theme) featuring weaponised Roomba-like cleaning robots unleashing violence upon one another when left alone in an unsuspecting owner's apartment.
+                        </p>
+                        <p>
+                            While the game has a fairly limited scope and no striking visuals, for example, it taught me a lot about multiple facets of indie game development, and served as a lesson in identifying common errors,
+                            not only in terms of code/technical implementation, but also project management, etc.
+                        </p>
+                        <p>
+                            I've also used this as an opportunity to learn some aspects of the engine that were new to me. For example, since the Roombas are round objects, I found it could be difficult for players to
+                            assess their current orientation in the world. As such, I've made use of the Unity ShaderGraph to create a "direction arrow" shader that increases its brightness and becomes smaller when it
+                            faces towards the player camera - the shift in size and brightness is meant to communicate that the player might no longer see the direction they're heading in.
+                        </p>
+                        <div class="col-1 col-md-2 col-xl-3"></div>
+                        <div class="col-10 col-md-8 col-xl-6">
+                            <video src="/assets/sinister roomba.mp4" class="w-100" preload="metadata" muted autoplay loop controls>
+                        </div>
+                        <div class="col-1 col-md-2 col-xl-3"></div>
+                        <p class="mt-3">
+                            I also integrated the game's audio with the different game systems, such as character control. For example, the Roomba's motor sound loop is modulated by its velocity.
+                            The game also takes advantage of Unity's 3D audio sources to ensure sounds are believably grounded in the scene.
+                        </p>
+                        <div class="col-1 col-md-2 col-xl-3"></div>
+                        <div class="col-10 col-md-8 col-xl-6">
+                            <video src="/assets/roomba cannibalism.mp4" class="w-100" preload="metadata" controls>
+                        </div>
+                        <div class="col-1 col-md-2 col-xl-3"></div>
+                        <p class="mt-3">
+                            The game also renders the guns' muzzle flashes with a single billboard quad and the below HLSL shader.
+                        </p>
+                        <div class="col-1 col-md-2 col-xl-3"></div>
+                        <div class="col-10 col-md-8 col-xl-6">
+                            <video src="/assets/billboard muzzle flash.mp4" class="w-100" preload="metadata" muted autoplay loop controls>
+                        </div>
+                        <div class="col-1 col-md-2 col-xl-3"></div>
+                        <div class="col-1 col-md-2"></div>
+                        <div class="col-10 col-md-8">
+                        <pre class="mt-3 hlsl-code border border-primary rounded p-3">
+float4 _InnerColor, _OuterColor;
+
+// World-space direction vector that the gun barrel is pointing in.
+float3 _MuzzleFlashDirection;
+
+// InvLerp. Thanks Freya Holmer, you're a lifesaver :P
+float inverseLerp(float val, float min, float max)
+{
+    return (val - min) / (max - min);
+}
+
+// The structure definition defines which variables it contains.
+// This example uses the Attributes structure as an input structure in
+// the vertex shader.
+struct Attributes
+{
+    // The positionOS variable contains the vertex positions in object
+    // space.
+    float4 positionOS   : POSITION;
+    float2 uv           : TEXCOORD0;
+};
+
+struct Varyings
+{
+    // The positions in this struct must have the SV_POSITION semantic.
+    float4 positionHCS  : SV_POSITION;
+    float2 uv : TEXCOORD0;
+};
+
+// The vertex shader definition with properties defined in the Varyings
+// structure. The type of the vert function must match the type (struct)
+// that it returns.
+Varyings vert(Attributes IN)
+{
+    // Declaring the output object (OUT) with the Varyings struct.
+    Varyings OUT;
+    // The TransformObjectToHClip function transforms vertex positions
+    // from object space to homogenous clip space.
+    OUT.positionHCS = TransformObjectToHClip(IN.positionOS.xyz);
+    // Provide UV coords to the fragment shader.
+    OUT.uv = IN.uv;
+    // Returning the output.
+    return OUT;
+}
+
+// Muzzle flash flame colour based on distance from centre point.
+half4 flashColour(float distance, float outlineDistance, float maxDistance)
+{
+    float innerFactor = inverseLerp(maxDistance - distance, 0.0, outlineDistance);
+    float outerFactor = inverseLerp(maxDistance - distance, 0.0, maxDistance);
+
+    half4 innerCol = _InnerColor * half4(1, 1, 1, innerFactor);
+    half4 outerCol = _OuterColor * half4(1, 1, 1, outerFactor);
+
+    // Defining the color variable and returning it.
+    return lerp(outerCol, innerCol, distance > maxDistance ? 0.0 : max(innerCol.a, outerCol.a));
+}
+
+// Colour when muzzle flash is aligned on the Z-axis
+half4 facingFront(float2 uv)
+{
+    float dist = distance(uv, float2(0.5, 0.5));
+
+    return flashColour(dist, 0.1, 0.5);
+}
+
+// Colour when muzzle flash direction is perpendicular to camera on Z-axis (aligned on X-axis).
+half4 facingSide(float2 uv, float xOrientation)
+{
+    float sideFactor = inverseLerp(xOrientation, -1, 1);
+    sideFactor = xOrientation;
+
+    float dist[3] = {
+        distance(uv, float2(0.5 + 0.15 * sideFactor, 0.38)),
+        distance(uv, float2(0.5 + 0.15 * sideFactor, 0.62)),
+        distance(uv, float2(0.5 - 0.2 * sideFactor, 0.5))
+    };
+
+    // Modulate size over time.
+    float wave = lerp(0.97, 1, abs(sin(_Time * 400)));
+    float waveSmall = lerp(0.99, 1, abs(sin(_Time * 400)));
+
+    return
+        flashColour(dist[0], 0.04, 0.2 * waveSmall) +
+        flashColour(dist[1], 0.04, 0.2 * waveSmall) +
+        flashColour(dist[2], 0.1, 0.5 * wave);
+}
+
+// The fragment shader definition.
+half4 frag(Varyings IN) : SV_Target
+{
+    // Ignore Y-axis as we want facingSide to also apply when viewed from above.
+    float3 camDir = float3(UNITY_MATRIX_V[0][0], 0, UNITY_MATRIX_V[2][0]);
+
+    // Get XZ-plane angle.
+    float orientation = dot(camDir, _MuzzleFlashDirection);
+
+    // Determine final colour.
+    half4 muzzleFlash = lerp(facingSide(IN.uv, -orientation), facingFront(IN.uv), 1 - abs(orientation));
+
+    // Modulate alpha over time to allow slight flicker in full-auto/burst.
+    muzzleFlash.a *= lerp(0.1, 0.3, max(0, sin(_Time * 800)));
+
+    return muzzleFlash;
+}
+                        </pre>
+                        </div>
+                        <div class="col-1 col-md-2"></div>
+                    `
                 }
             ]
         })
