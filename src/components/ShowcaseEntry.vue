ccc7d04ae4a15efd08ec7cdebac8ad92b964c5d3
Tomasz Zając <tomezpl@gmail.com>
2022-03-15 20:36:04 +0000

    grid in showcaseentry so tag badges are more readable

--- src/components/ShowcaseEntry.vue
+++ src/components/ShowcaseEntry.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="showcase-entry">
-        <h3>
-            <a v-bind:href="githubLink" target="_blank" v-if="githubLink !== ''">
-                {{title}}
-                <i class="bi-box-arrow-up-right small-icon align-top"></i>
-            </a>
-            <span v-if="githubLink === ''">{{title}}</span>
-        </h3>
-        <p>
-            <span class="badge bg-success mx-1" v-for="tag in tags" :key="tag">{{tag}}</span>
-        </p>
-        <slot></slot>
+        <div class="row">
+            <div class="col-12">
+                <h3>
+                    <a v-bind:href="githubLink" target="_blank" v-if="githubLink !== ''">
+                        {{title}}
+                        <i class="bi-box-arrow-up-right small-icon align-top"></i>
+                    </a>
+                    <span v-if="githubLink === ''">{{title}}</span>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-11 col-md-8 col-lg-6">
+                <span class="badge bg-success mx-1" v-for="tag in tags" :key="tag">{{tag}}</span>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <slot></slot>
+        </div>
         <hr class="blog-post-separator mt-4 mb-3 w-100" />
     </div>
 </template>
