14d805ce5d4c14359e778b0b8fd399108ea139ec
Tomasz Zając <tomezpl@gmail.com>
2022-03-22 18:41:44 +0000

    separated component

--- src/components/PlannerComponent.vue
+++ src/components/PlannerComponent.vue
@@ -1,20 +1,13 @@
 <template>
         <div class="row p-0 m-3">
             <div class="col-12 planner-timeline p-0 m-0 bg-dark">
+                <div class="row p-0 m-0 planner-timeline-axis">
+                    <PlannerDayTimeline axis />
+                </div>
                 <div class="row p-0 m-0">
-                    <div class="col-1 planner-timeline-bihour p-0 m-0" v-for="n in 12" :key="n">
-                        <div class="row p-0 m-0">
-                            <div class="col-6 planner-timeline-hour p-0 m-0" v-for="m in 2" :key="m">
-                                <div class="row m-0 p-0">
-                                    <div class="col-12 p-0 m-0">{{(n-1)*2 + m - 1}}</div>
-                                    <div class="col-12 m-0 p-0">
-                                        <div class="col-3 d-inline-block planner-timeline-quarter-notch p-0 m-0" v-for="i in 3" :key="i">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <PlannerDayTimeline>
+
+                    </PlannerDayTimeline>
                 </div>
             </div>
         </div>
@@ -22,10 +15,14 @@
 
 <script lang="ts">
     import { defineComponent } from 'vue';
+    import PlannerDayTimeline from './planner/core/PlannerDayTimeline.vue';
     import '../css/planner.css';
 
     export default defineComponent({
         name: 'PlannerComponent',
+        components: {
+            PlannerDayTimeline
+        },
         data: () => ({
 
         })
---
+++ src/components/planner/core/PlannerDayTimeline.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="col-1 planner-timeline-bihour p-0 m-0" v-for="n in 12" :key="n">
+        <div class="row p-0 m-0">
+            <div class="col-6 planner-timeline-hour p-0 m-0" v-for="m in 2" :key="m">
+                <div class="row m-0 p-0">
+                    <div class="col-12 p-0 m-0">
+                        <slot :bihour="n" :hourpart="m" :hour="toHour(n, m)">
+                            <span v-show="axis">{{(n-1)*2 + m - 1}}</span>
+                        </slot>
+                    </div>
+                    <div class="col-12 m-0 p-0">
+                        <div class="col-3 planner-timeline-quarter-notch p-0 m-0" v-for="i in 3" :key="i">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'PlannerDayTimeline',
+        props: {
+            axis: {
+                type: Boolean,
+                default: false
+            }
+        },
+        methods: {
+            toHour(n: number, m: number): number {
+                return (n - 1) * 2 + m - 1;
+            }
+        }
+    });
+</script>
