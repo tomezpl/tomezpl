a3c9e718ae25dbbca569ab50d2aa18a00d86b4ad
Tomasz Zając <tomezpl@gmail.com>
2022-09-16 13:56:04 +0100

    early navbar

--- src/Home.vue
+++ src/Home.vue
@@ -1,20 +1,48 @@
 <template>
-<div class="container-fluid text-light">
-    <div class="navbar navbar-expand-lg navbar-light bg-light">
-
+  <div class="container-fluid p-0">
+    <div class="navbar p-3 navbar-expand-lg navbar-dark bg-primary">
+      <h1 class="navbar-brand">Tomasz Zając</h1>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarContent">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Showcase</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <h1>Tomasz Zajac</h1>
-</div>
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "css/styles.scss";
 </style>
 
 <script lang="ts">
-    import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
-    export default defineComponent({
-        name: 'Home'
-    })
+export default defineComponent({
+  name: "Home",
+});
 </script>
