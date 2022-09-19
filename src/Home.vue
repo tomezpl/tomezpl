3f519d9681e57151ce3e0f892b76f4fb5c3f6dec
Tomasz Zając <tomezpl@gmail.com>
2022-09-19 03:12:55 +0100

    colours

--- src/Home.vue
+++ src/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="container-fluid p-0">
     <div class="row sticky-top">
-      <div class="navbar p-0 navbar-expand-md navbar-dark tz-brand-gradient">
+      <div
+        class="
+          navbar
+          p-0
+          navbar-expand-md navbar-dark
+          tz-brand-gradient
+          tz-brand-border
+          border-2 border-bottom
+          shadow-lg
+        "
+      >
         <div class="navbar-brand col-10 col-md-2 me-0 mb-0 py-0">
           <div class="py-2">
             <span class="w-100 mx-auto d-block text-center fs-4">
@@ -20,35 +30,73 @@
         <div class="collapse navbar-collapse p-0" id="navbarContent">
           <ul class="navbar-nav mb-2 mb-md-0">
             <li class="nav-item px-3 my-md-0 my-1 mt-2 py-2">
-              <a class="nav-link active btn-tznav text-center fw-bold shadow px-1" aria-current="page" href="#">Home</a>
+              <a
+                class="
+                  nav-link
+                  active
+                  btn-tznav
+                  text-center
+                  fw-bold
+                  shadow
+                  px-1
+                "
+                aria-current="page"
+                href="#"
+                >Home</a
+              >
             </li>
             <li class="nav-item px-3 my-md-0 my-1 py-2">
-              <a class="nav-link btn-tznav text-center fw-bold shadow px-1" href="#">Blog</a>
+              <a
+                class="nav-link btn-tznav text-center fw-bold shadow px-1"
+                href="#"
+                >Blog</a
+              >
             </li>
             <li class="nav-item px-3 my-md-0 my-1 py-2">
-              <a class="nav-link btn-tznav text-center fw-bold shadow px-1" href="#">Showcase</a>
+              <a
+                class="nav-link btn-tznav text-center fw-bold shadow px-1"
+                href="#"
+                >Showcase</a
+              >
             </li>
             <li class="nav-item px-3 my-md-0 my-1 py-2">
-              <a class="nav-link btn-tznav text-center fw-bold shadow px-1" href="#">About</a>
+              <a
+                class="nav-link btn-tznav text-center fw-bold shadow px-1"
+                href="#"
+                >About</a
+              >
             </li>
           </ul>
         </div>
       </div>
     </div>
 
-    <div class="row min-vh-100 bg-gradient pt-5">
+    <div class="row min-vh-100 side-bg pt-5">
       <div class="col-md-2 col-sm-1 d-sm-block d-none"></div>
-      <div
-        class="
-          col-md-8 col-sm-10 col-12
-          p-4
-          bg-dark
-          border border-top-0 border-2 border-dark
-        "
-      >
-        <div class="col-7 bg-light p-2">
-          <h1>Tomasz Zając</h1>
-          <h2>Software engineer</h2>
+      <div class="col-md-8 col-sm-10 col-12">
+        <div class="col-12 main-page-content p-4">
+          <div class="col-7 bg-light p-2">
+            <h1>Tomasz Zając</h1>
+            <h2>Software engineer</h2>
+          </div>
+        </div>
+        <div class="col-12 main-page-content p-4">
+          <div class="col-7 bg-light p-2">
+            <h1>Tomasz Zając</h1>
+            <h2>Software engineer</h2>
+          </div>
+        </div>
+        <div class="col-12 main-page-content p-4">
+          <div class="col-7 bg-light p-2">
+            <h1>Tomasz Zając</h1>
+            <h2>Software engineer</h2>
+          </div>
+        </div>
+        <div class="col-12 main-page-content p-4">
+          <div class="col-7 bg-light p-2">
+            <h1>Tomasz Zając</h1>
+            <h2>Software engineer</h2>
+          </div>
         </div>
       </div>
       <div class="col-md-2 col-sm-1 d-sm-block d-none"></div>
