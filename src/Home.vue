6c66316f91d56e26d56bf51c1bb5e67e4b968141
Tomasz Zając <tomezpl@gmail.com>
2022-10-05 23:43:21 +0100

    cover

--- src/Home.vue
+++ src/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="col-12 main-page-content p-4 home-intro-cover">
-    <div class="clearfix">
-    <div class="col-lg-5 col-12 float-end text-light">Test</div>
-    <div class="col-lg-7 col-12 text-light p-2 float-start" id="home-intro-cover-brief">
+  <div
+    class="
+      col-12
+      main-page-content
+      p-1
+      p-sm-4
+      home-intro-cover
+    "
+  >
+    <div
+      class="text-light p-2 clearfix"
+      id="homeIntroCoverBrief"
+    >
+      <img class="float-end ms-3" style="max-height: 15rem; min-height: 8rem" src="./assets/glowy_portrait.png"/>
       <h1>Tomasz Zając</h1>
-      <h4>An experienced, self-motivated software developer, with a wide array of skills ranging from Web API programming to implementing interaction-heavy UI systems, graphics rendering and physics simulation. I focus on delivering the best product possible, promote good practice and aim to foster a friendly &amp; helpful atmosphere while working as part of a team.</h4>
+      <h4>
+        An experienced, self-motivated software developer, with a wide array of
+        skills ranging from Web API programming to implementing
+        interaction-heavy UI systems, graphics rendering and physics simulation.
+        I focus on delivering the best product possible, promote good practice
+        and aim to foster a friendly &amp; helpful atmosphere while working as
+        part of a team.
+      </h4>
     </div>
-  </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import './css/home.scss';
+import "./css/home.scss";
 
 export default defineComponent({
   name: "Home",
