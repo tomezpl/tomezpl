1263f60e5fbe1fadb2268db6b6c7220b517319ab
Tomasz Zając <tomezpl@gmail.com>
2022-10-16 20:52:24 +0100

    cleanup

--- src/Home.vue
+++ src/Home.vue
@@ -51,7 +51,7 @@
             :popover-title="tech.title"
             :popover-text="tech.brief"
             :icon="tech.icon"
-          ></ButtonWithPopover>
+          />
           <!-- Web technologies will go here -->
         </div>
       </div>
@@ -74,7 +74,7 @@
             :popover-title="tech.title"
             :popover-text="tech.brief"
             :icon="tech.icon"
-          ></ButtonWithPopover>
+          />
           <!-- Game technologies will go here -->
         </div>
         <p class="fs-5 ps-3">
@@ -107,7 +107,7 @@
 </style>
 
 <script lang="ts">
-import { defineComponent, h } from "vue";
+import { defineComponent } from "vue";
 import ButtonWithPopover from "./components/home/ButtonWithPopover.vue";
 
 export default defineComponent({
@@ -163,28 +163,31 @@ export default defineComponent({
     gameTechExperience() {
       return {
         unreal: {
-          title: 'Unreal Engine',
-          brief: '1 year of using Unreal Engine 4 and 5 for small personal game projects. Familiar with both C++ and Blueprint programming, as well as basics of setting up scene lighting using Lumen.',
-          icon: require('./assets/ue.png')
+          title: "Unreal Engine",
+          brief:
+            "1 year of using Unreal Engine 4 and 5 for small personal game projects. Familiar with both C++ and Blueprint programming, as well as basics of setting up scene lighting using Lumen.",
+          icon: require("./assets/ue.png"),
         },
         unity: {
-          title: 'Unity',
+          title: "Unity",
           brief: `Familiar with using the Unity engine, having used it on-and-off since 2013, including 1 year spent on developing a small multiplayer game to completion.
           Familiar with C# programming, Photon & Netcode for GameObjects, Universal Render Pipeline, Shader Graph and HLSL.`,
-          icon: require('./assets/unityLogo.png')
+          icon: require("./assets/unityLogo.png"),
         },
         opengl: {
-          title: 'OpenGL',
-          brief: 'Familiar with creating basic 3D renderers using the modern OpenGL spec using C++ for host code, writing GLSL shaders for simple lighting models (Gouraud, Phong), and debugging using ImGui and RenderDoc.',
-          icon: require('./assets/opengl.png')
+          title: "OpenGL",
+          brief:
+            "Familiar with creating basic 3D renderers using the modern OpenGL spec using C++ for host code, writing GLSL shaders for simple lighting models (Gouraud, Phong), and debugging using ImGui and RenderDoc.",
+          icon: require("./assets/opengl.png"),
         },
         physx: {
-          title: 'PhysX',
-          brief: 'Working knowledge of the NVIDIA PhysX C++ SDK. As part of University project, produced a small 3D pinball game making use of various collider types, different material properties (restitution/friction coefficients) to serve gameplay purposes, and integrated simulation with a basic OpenGL scene graph.',
-          icon: require('./assets/physx.png')
-        }
-      }
-    }
+          title: "PhysX",
+          brief:
+            "Working knowledge of the NVIDIA PhysX C++ SDK. As part of University project, produced a small 3D pinball game making use of various collider types, different material properties (restitution/friction coefficients) to serve gameplay purposes, and integrated simulation with a basic OpenGL scene graph.",
+          icon: require("./assets/physx.png"),
+        },
+      };
+    },
   },
 });
 </script>
