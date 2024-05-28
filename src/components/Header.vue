8cfcc217547d572f51b4f3aeec75fa9d36fe84d0
Tomasz Zajac <tomezpl@gmail.com>
2024-05-28 13:35:13 +0100

    chore(Header): removed unused code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,3 @@
-<!--<template>
-    <div class="row pt-3 d-flex">
-        <div class="col-3 col-sm-2 pb-3 text-center align-self-center">
-            <img src="../assets/tz_logo.png" class="img-fluid tz-logo" />
-        </div>
-        <div class="col d-none d-sm-block">
-
-        </div>
-        <div class="col-xl-5 col-9 col-sm-8 pb-3 text-center align-self-center">
-            <h1 class="align-middle">
-                Tomasz Zaj&#261;c - devblog
-            </h1>
-        </div>
-        <div class="col-xl-4 col-12 px-0 align-self-end">
-            <ul class="nav justify-content-end text-uppercase fw-bold">
-                <li class="nav-item">
-                    <a class="nav-link w-100 link-light" v-bind:class="{ active: path === '/' || path === '/index.html'}" :aria-current="path === '/' || path === '/index.html' ? 'page' : ''" href="/">
-                        Home
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link link-light" v-bind:class="{ active: path === '/about.html'}" :aria-current="path === '/about.html' ? 'page' : ''" href="/about.html">
-                        About me
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link link-light" v-bind:class="{ active: path === '/showcase.html'}" :aria-current="path === '/showcase.html' ? 'page' : ''" href="/showcase.html">
-                        Showcase
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</template>
--->
-
 <template>
   <div
     class="tz-brand-gradient"
